7fdc1021b9cb12acc1f38e87d9d45332ac63075e
Onwukwennamdi <nnamdionwukwe@gmail.com>
2026-08-04 12:39:17 +0100

    dscds

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -58,7 +58,7 @@ export default function Footer() {
           © {new Date().getFullYear()} Sugar Cocktail Bar. Made with{" "}
           <FaHeart className={styles.heart} /> in Abuja.
         </p>
-        <p>Powered By GesTechCom</p>
+        <p>Powered By GesTechCom Technologies</p>
       </div>
     </footer>
   );
